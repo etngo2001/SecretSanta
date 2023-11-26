df4c02ed1b9a5da90d85198bec95714859b9f3e7
Julian Ngo <jultngo@gmail.com>
2023-11-25 20:38:00 -0800

    added post request functionality, broke up views

--- server.js
+++ server.js
@@ -19,14 +19,34 @@ app.get("/", (req, res) => {
     res.render('index');
     console.log('rendering index page');
 })
+app.post("/", (req, res) => {
+    res.render('index');
+    console.log('rendering index page');
+})
 
-app.get("/guessSS", (req, res) => {
+app.post("/SSForm", (req, res) => {
+    if(req.body.agreedge) { //checks to see if user is able to move onto the form page
+        res.render('SSForm');
+        console.log('rendering SSForm page');
+    }
+    res.status(204).send(); //keeps user on current page
+})
+
+app.post("/viewSS", (req, res) => {
+    res.render('viewSS');
+    console.log('rendering ViewSS page');
+})
+
+app.post("/guessSS", (req, res) => {
     res.render('guessSS');
     console.log('rendering Guess Secret Santa page');
 })
 
-app.post("/send-list", (req, res) => { //triggered on ssform when they submit form, need to run check before doing updating json form
-    req
+
+
+app.post("/submit-list", (req, res) => { //triggered on ssform when they submit form, need to run check before doing updating json form
+    console.log(req.body.name);
+    console.log("form submitted")
 })
 
 //TODO:
